9e1a4f89a6025651f66817b904c447c703ed4862
avisek987 <57721543+avisek987@users.noreply.github.com>
2021-12-31 18:17:37 +0530

    Create BubbleSortmp.java

---
+++ BubbleSortmp.java
@@ -0,0 +1,18 @@
+package com.lab4.com.service;
+
+public class BubbleSortImp {
+public void bubbleSort(int notes[]) {
+	for(int i=0; i<notes.length;i++) {
+		for(int j=0; j<notes.length-i-1;j++) {
+			if(notes[j]<notes[j+1]) {
+				int temp = notes[j];
+				notes[j]=notes[j+1];
+				notes[j+1]=temp;
+			}
+			
+		}
+	}
+	 
+
+}
+}
